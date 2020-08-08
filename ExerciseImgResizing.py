3b8400be2a488f652595cdfd6d195b1cc7043120
Laura <45018127+LauraGnc@users.noreply.github.com>
2020-08-08 09:54:39 +0100

    Add files via upload
    Adding a new version of my script to resize images (any format)

--- ExerciseImgResizing.py
+++ ExerciseImgResizing.py
@@ -3,14 +3,19 @@
 specify the formats of the images. If you want the images to be resized and to be
 in black and white, change the value in line 14 from 1 to 0.'''
 
+# The picture(s) need to be in the same directory as the script.
+
 
 import cv2
 import glob
 
 # Store the img in a LIST:
-listi = glob.glob('*.jpg')
+listi = glob.glob('*.png')
 
 for i in listi:
-    img = cv2.imread(i, 0) # [0] for B&W ||| 1 to keep the colors.
-    re = cv2.resize(img, (200, 200)) # Change the resized values here!
+    img = cv2.imread(i, 1) # [0] for B&W ||| 1 to keep the colors.
+    # re = cv2.resize(img, (500, 300)) # Change the resized values here!
+
+    # Resize your image by percentage with the following snippet
+    re = cv2.resize(img, (int(img.shape[1]/4), int(img.shape[0]/4)))
     nw = cv2.imwrite('resized_'+i, re)
